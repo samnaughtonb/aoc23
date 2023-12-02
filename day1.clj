009eee10d2b1c564b292392c8c3e4fa0a013c5e5
Samuel Naughton Baldwin <sam.naughton2000@gmail.com>
2023-12-02 16:06:12 +0000

    Add docstrings for Day 1 and Day 2 submissions

--- day1.clj
+++ day1.clj
@@ -1,7 +1,57 @@
 (ns day1
   (:require [clojure.string :as str]))
 
-(def digit-map-simple
+(defn first-posn
+  "From an input string and a vector of substring against which
+  to match, find the first index that matches any of them; if none
+  match, return nil"
+  [input matches]
+  (let [raw-posns (map #(str/index-of input %) matches)
+        act-posns (filter #(not (nil? %)) raw-posns)]
+    (cond (> (count act-posns) 0) (apply min act-posns)
+          :else nil)))
+
+(defn first-num
+  "Find the first instance of a number in string input,
+  where `mapping` maps a vector of matching strings to an integer"
+  [mapping input]
+  (second
+    (reduce (fn [acc [matches value]]
+      (let [[fst num] acc
+            posn (first-posn input matches)]
+        (cond (or (nil? posn) (>= posn fst)) acc
+              :else [posn value])))
+      [(+ 1 (count input)) nil] ; initial reduction value
+      mapping)))
+
+(defn reverse-mapping
+  "Mapping of the form `{ ['hello' 'world'] 69 ... }` are reversed to
+  obtain new mappings of the form `{ ['olleh' 'dlrow'] 69 ... }`"
+  [mapping]
+  (zipmap (map #(map str/reverse %) (keys mapping)) (vals mapping)))
+
+(defn calibration-value
+  "Determine first and last matching digits (as per `mapping`) in `input`
+  string then 'concatenate', which is equivalent to multiplying the first
+  by 10 and summing the second (since they're going to be single digits)"
+  [mapping input]
+  (let [fst (first-num mapping input)
+        lst (first-num (reverse-mapping mapping) (str/reverse input))]
+    (+ (* 10 fst) lst)))
+
+(defn total-calibration-value [contents mapping]
+  "Sum the 'calibration values' determined from each line of input"
+  (->> contents
+       (map #(calibration-value mapping %))
+       (reduce +)))
+
+(def contents
+  (->> "INPUT_FILE"
+       System/getenv
+       slurp
+       str/split-lines))
+
+(def digit-map-part1
   {["0"] 0
    ["1"] 1
    ["2"] 2
@@ -13,7 +63,10 @@
    ["8"] 8
    ["9"] 9})
 
-(def digit-map-hard
+(println (format "Problem 1: %d"
+                 (total-calibration-value contents digit-map-part1)))
+
+(def digit-map-part2
   {["zero" "0"]  0
    ["one" "1"]   1
    ["two" "2"]   2
@@ -25,46 +78,5 @@
    ["eight" "8"] 8
    ["nine" "9"]  9})
 
-(defn first-posn [input nums]
-  (let [raw-posns (map #(str/index-of input %) nums)
-        act-posns (filter #(not (nil? %)) raw-posns)]
-    (cond (> (count act-posns) 0) (apply min act-posns)
-          :else nil)))
-
-(defn first-num [mapping input]
-  (defn my-func [acc [matches value]]
-    (let [[fst num] acc
-          wtf (first-posn input matches)]
-      (cond (or (nil? wtf) (>= wtf fst)) acc
-            :else [wtf value])))
-  (second (reduce my-func
-                  [(+ 1 (count input)) nil]
-                  mapping)))
-
-(defn reverse-mapping [mapping]
-  (zipmap (map #(map str/reverse %) (keys mapping)) (vals mapping)))
-
-(println (reverse-mapping digit-map-simple))
-(println (reverse-mapping digit-map-hard))
-
-(defn calibration-value [mapping input]
-  (let [fst (first-num mapping input)
-        lst (first-num (reverse-mapping mapping) (str/reverse input))]
-    (+ (* 10 fst) lst)))
-
-(defn total-calibration-value [contents mapping]
-  (->> contents
-       (map #(calibration-value mapping %))
-       (reduce +)))
-
-(def contents
-  (->> "INPUT_FILE"
-       System/getenv
-       slurp
-       str/split-lines))
-
-(println (format "Problem 1: %d"
-                 (total-calibration-value contents digit-map-simple)))
-
 (println (format "Problem 2: %d"
-                 (total-calibration-value contents digit-map-hard)))
+                 (total-calibration-value contents digit-map-part2)))
--- day2.clj
+++ day2.clj
@@ -1,31 +1,52 @@
 (ns day2
   (:require [clojure.string :as str]))
 
-(defn parse-col [col]
+(defn parse-col
+  "Create data structure (map) from an input string, e.g.
+  the string '7 green' would go to {:g 7}"
+  [col]
   (let [[numstr colour] (str/split col #" ")
         num (Integer/parseInt numstr)]
     (cond (= colour "green") {:g num}
           (= colour "red") {:r num}
           (= colour "blue") {:b num})))
 
-(defn parse-hand [hand]
+(defn parse-hand
+  "Map a hand (i.e. a list of strings) to a map representing
+  the contents thereof, defaulting to zero for colours that
+  don't appear in the hand, e.g. '7 green, 2 blue' would map
+  to {:r 0 :g 7 :b 2}"
+  [hand]
   (def defaults {:r 0 :g 0 :b 0})
   (apply merge (conj (map parse-col hand) defaults)))
 
-(defn parse-game [idx line]
+(defn parse-game
+  "Given an enumerated line string, return a data structure of
+  the form (123 ({:r 42 ...} {:r 69 ...})) representing the game
+  number and the list of hands in the game"
+  [idx line]
   (let [raw-hands (str/split (second (str/split line #": ")) #"; ")
         hands (map #(str/split % #", ") raw-hands)]
     (list (+ idx 1) (map parse-hand hands))))
 
-(defn maximal-hand [hands]
+(defn maximal-hand
+  "Find colour-wise maximum given a list of hands"
+  [hands]
   (let [colours (list :r :g :b)]
     (zipmap colours (map #(apply max (map % hands)) colours))))
 
-(defn valid-bag? [bounds max-hand]
+(defn valid-bag?
+  "Test whether or not a hand satisfies the colour-wise maximum
+  constraints specified by `bounds`"
+  [bounds hand]
   (let [colours (list :r :g :b)]
-    (every? #(<= (% max-hand) (% bounds)) colours)))
+    (every? #(<= (% hand) (% bounds)) colours)))
 
-(defn valid-game? [bounds game]
+(defn valid-game?
+  "Test whether or a game satisfies the colour-wise maximum
+  constraints specified by `bounds`, that is, whether or not
+  the colour-wise maximal hand satisfies the bounds"
+  [bounds game]
   (let [[idx hands] game
         max-hand (maximal-hand hands)]
     (valid-bag? bounds max-hand)))
@@ -42,17 +63,28 @@
    :g 13
    :b 14})
 
-(println (format "Problem 1: %d"
+(defn total-idcs-valid-games
+  "(Part 1) Filter the games by which are valid under the specified
+  bounds, and sum their game numbers"
+  [bounds games]
   (->> games
        (filter #(valid-game? bounds %))
        (map first)
-       (reduce +))))
+       (reduce +)))
 
-(println (format "Problem 2: %d"
+(defn total-product-maximal-hands
+  "(Part 2) Find the maximal hand for each of the games (obtained by
+  doing colour-wise maximum on all the hands in a game) and take the
+  sum-product of the values across all games (no filtering as in part 1)"
+  [games]
   (->> games
        (map second)
        (map maximal-hand)
        (map #(apply * (vals %)))
-       (reduce +))))
+       (reduce +)))
 
+(println (format "Problem 1: %d"
+                 (total-idcs-valid-games bounds games)))
 
+(println (format "Problem 2: %d"
+                 (total-product-maximal-hands games)))
